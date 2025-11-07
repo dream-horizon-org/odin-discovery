5a92931aa1e9d4bd5a61d0ae0c9dfff847808e94
Aakash Tiwari <aakash.tiwari@dream11.com>
2025-11-07 16:19:49 +0530

    chore: precommit checks

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,4 +5,5 @@ JAVA_OPTS="-XX:+HeapDumpOnOutOfMemoryError"
 JMX_OPTS="-Dcom.sun.management.jmxremote=true -Dcom.sun.management.jmxremote.port=1099 -Dcom.sun.management.jmxremote.authenticate=false -Dcom.sun.management.jmxremote.ssl=false"
 APP_OPTS="-Dvertx.disableDnsResolver=true"
 export JVM_OPTS=${JVM_OPTS:-""}
+#shellcheck disable=SC2086
 exec java -jar ${JAVA_OPTS} ${JMX_OPTS} ${APP_OPTS} ${JVM_OPTS} odin-discovery-service-fat.jar
